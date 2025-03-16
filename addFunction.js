de3930a4d148e26759ef01029e838b877da2b68a
Prasanna-cpu <76141090+Prasanna-cpu@users.noreply.github.com>
2025-03-16 16:15:21 +0530

    single commit
    * add-revert-console-log
    
    * revert-real
    
    * commit
    
    * bug-fixes-in-add
    
    * absolute add

--- addFunction.js
+++ addFunction.js
@@ -1,6 +1,18 @@
 function addNumbers(a,b){
+    if(a == null || b == null || typeof a !== "number" || typeof b !== "number"){
+        return
+    }
     return a+b
 }
 
 
 console.log(addNumbers(3,4))
+
+
+function addAndReturnModulus(a,b){
+    if(a == null || b == null || typeof a !== "number" || typeof b !== "number"){
+        return
+    }
+    return Math.abs(a+b)
+}
+
