ddb99e55d61666fdc0b128f52e8f3287186abf71
Prasanna-cpu <kumar22maran@gmail.com>
2025-03-16 16:07:04 +0530

    absolute add

--- addFunction.js
+++ addFunction.js
@@ -9,3 +9,10 @@ function addNumbers(a,b){
 console.log(addNumbers(3,4))
 
 
+function addAndReturnModulus(a,b){
+    if(a == null || b == null || typeof a !== "number" || typeof b !== "number"){
+        return
+    }
+    return Math.abs(a+b)
+}
+
