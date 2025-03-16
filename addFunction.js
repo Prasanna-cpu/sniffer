cd605533d8e922126df9cee3042a6d9b09a5984d
Prasanna-cpu <kumar22maran@gmail.com>
2025-03-16 16:04:06 +0530

    bug-fixes-in-add

--- addFunction.js
+++ addFunction.js
@@ -1,6 +1,11 @@
 function addNumbers(a,b){
+    if(a == null || b == null || typeof a !== "number" || typeof b !== "number"){
+        return
+    }
     return a+b
 }
 
 
 console.log(addNumbers(3,4))
+
+
